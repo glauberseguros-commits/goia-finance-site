6e5eb44394a2781c8bcac6224261dc70ab9fae38
glauberseguros-commits <glauber.seguros@gmail.com>
2026-06-26 10:44:27 -0300

    Cria dashboard controller e service do Admin

---
+++ controllers/admin_dashboard_controller.py
@@ -0,0 +1,12 @@
+from controllers.admin_controller import AdminController
+
+
+class AdminDashboardController:
+
+    @staticmethod
+    def metricas():
+        return AdminController.metricas()
+
+    @staticmethod
+    def assinantes():
+        return AdminController.assinantes()
---
+++ utils/services/admin_dashboard_service.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+from controllers.admin_dashboard_controller import AdminDashboardController
+
+
+def obter_dashboard_admin():
+
+    metricas = AdminDashboardController.metricas()
+
+    assinantes = AdminDashboardController.assinantes()
+
+    if isinstance(assinantes, pd.DataFrame):
+        total = len(assinantes)
+    else:
+        total = 0
+
+    return {
+        "metricas": metricas,
+        "assinantes": assinantes,
+        "total_assinantes": total,
+    }
