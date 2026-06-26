f7f936cc2785b31060cd84100331b569592c42d8
glauberseguros-commits <glauber.seguros@gmail.com>
2026-06-26 10:13:12 -0300

    Cria controller dedicado para auditoria administrativa

---
+++ controllers/admin_auditoria_controller.py
@@ -0,0 +1,9 @@
+from utils.services.admin_service import (
+    listar_auditoria_admin,
+)
+
+class AdminAuditoriaController:
+
+    @staticmethod
+    def listar(limite=100):
+        return listar_auditoria_admin(limite)
